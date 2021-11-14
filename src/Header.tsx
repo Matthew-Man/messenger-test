14440cf5bf6531eb2072ac794cbd536029464797
Matthew Man <43723450+Matthew-Man@users.noreply.github.com>
2021-11-14 21:36:59 +0000

    Added user menu to header

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,10 +1,32 @@
-import { Box, Heading } from "@chakra-ui/react";
+import { Box, Flex, Heading } from "@chakra-ui/react";
+import { User } from "firebase/auth";
+import { useAuthState } from "react-firebase-hooks/auth";
 import Logo from "./components/Logo";
+import { auth } from "./utils/firebase";
 
 export default function Header() {
+  const [user] = useAuthState(auth);
   return (
-    <Box width="100%" padding={2}>
+    <Flex
+      justifyContent="space-between"
+      alignItems="center"
+      width="100%"
+      padding={2}
+    >
       <Logo width="200px" height="70px" />
-    </Box>
+      {user && (
+        <Box align="right">
+          <UserMenu user={user} />
+        </Box>
+      )}
+    </Flex>
+  );
+}
+
+function UserMenu({ user }: { user: User }) {
+  return (
+    <Heading as="h3" size="sm">
+      Hi, {user.displayName}
+    </Heading>
   );
 }
